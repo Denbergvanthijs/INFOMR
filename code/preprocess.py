73fb528f45712bde045af5e98f9b075d35377f84
Leonardo Trujillo <leonardo.trujillo.m@gmail.com>
2023-09-20 01:04:49 +0200

    Using pymeshlab for mesh stats. Compute avg shape.

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,12 +1,18 @@
 import os
 import numpy as np
 import csv
+import pymeshlab
+
+save_mesh_info = 0
 
 def read_meshes():
+    ms = pymeshlab.MeshSet()
+
     data_folder = 'data'
 
     # Subset of the data
-    categories = ['DeskLamp', 'Bottle', 'Skyscraper']
+    # categories = ['DeskLamp', 'Bottle', 'Skyscraper']
+    categories = next(os.walk(data_folder))[1]
 
     # Initial list to store all mesh info
     mesh_info = []
@@ -24,47 +30,67 @@ def read_meshes():
             mesh_path = os.path.join(category_folder, filename)
             
             if os.path.isfile(mesh_path):
-                '''
-                Commented stuff below is cleaner than the actual code below. 
-                Couldn't find a 'faces' attribute or anything.
-                Code below works fine, but not cool or optimal. I was lazy.
-                '''
-                # mesh = o3d.io.read_triangle_mesh(mesh_path)
-                # mesh.compute_vertex_normals()
-                # num_vertices = len(np.asarray(mesh.vertices))
+                ms.load_new_mesh(mesh_path)
+                m = ms.current_mesh()
 
-                vertices = []
-                faces = []
+                vertices = m.vertex_number()
+                faces = m.face_number()
+                bbox = m.bounding_box()
+                bbox_min = bbox.min()
+                bbox_max = bbox.max()
 
-                with open(mesh_path, 'r') as file:
-                    lines = file.readlines()
+                # Now we only store class label, num vertices and num faces. We should be more complete of course
+                mesh_info.append([category, vertices, faces, 
+                                  bbox_min[0], bbox_min[1], bbox_min[2], 
+                                  bbox_max[0], bbox_max[1], bbox_max[2]])
 
-                for line in lines:
-                    parts = line.strip().split()
+                # vertices = []
+                # faces = []
 
-                    if not parts:
-                        continue
+                # with open(mesh_path, 'r') as file:
+                #     lines = file.readlines()
 
-                    if parts[0] == 'v':
-                        vertex = [float(parts[1]), float(parts[2]), float(parts[3])]
-                        vertices.append(vertex)
-                    elif parts[0] == 'f':
-                        face = [int(v.split('/')[0]) - 1 for v in parts[1:]]  
-                        faces.append(face)
+                # for line in lines:
+                #     parts = line.strip().split()
 
-                # Now we only store class label, num vertices and num faces. We should be more complete ofcourse
-                mesh_info.append([category, len(vertices), len(faces)])
+                #     if not parts:
+                #         continue
+
+                #     if parts[0] == 'v':
+                #         vertex = [float(parts[1]), float(parts[2]), float(parts[3])]
+                #         vertices.append(vertex)
+                #     elif parts[0] == 'f':
+                #         face = [int(v.split('/')[0]) - 1 for v in parts[1:]]  
+                #         faces.append(face)
+
+                # # Now we only store class label, num vertices and num faces. We should be more complete ofcourse
+                # mesh_info.append([category, len(vertices), len(faces)])
 
     return np.array(mesh_info)
+
+def avg_shape(mesh_info):
+    n = len(mesh_info)
+    tot_vertices = 0
+    tot_faces = 0
+    for m in mesh_info:
+        tot_vertices += int(m[1])
+        tot_faces += int(m[2])
+    
+    return [tot_vertices / n, tot_faces / n]
 
 
 if __name__ == "__main__":
     mesh_info = read_meshes()
 
-    # Save mesh info to a CSV file
-    csv_file_path = "mesh_info.csv"
-    with open(csv_file_path, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['Class', 'Vertices', 'Faces'])
-        for row in mesh_info:
-             csv_writer.writerow(row)
+    if save_mesh_info:
+        # Save mesh info to a CSV file
+        csv_file_path = "data/mesh_info.csv"
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(['Class', 'Vertices', 'Faces', 
+                                'BB Min x', 'BB Min y', 'BB Min z', 
+                                'BB Max x', 'BB Max y', 'BB Max z'])
+            for row in mesh_info:
+                csv_writer.writerow(row)
+
+    print(avg_shape(mesh_info))
