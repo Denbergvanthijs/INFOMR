abef8a9f2160f986fd645ae6b97dca61dadac3b3
RiemerDijkstra <50399640+RiemerDijkstra@users.noreply.github.com>
2023-09-19 16:30:24 +0200

    Add files via upload

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,9 +1,17 @@
 import os
+import numpy as np
+import csv
 
-def main():
+def read_meshes():
     data_folder = 'data'
+
+    # Subset of the data
     categories = ['DeskLamp', 'Bottle', 'Skyscraper']
 
+    # Initial list to store all mesh info
+    mesh_info = []
+
+    # Iterate over all classes in data subset 
     for category in categories:
         category_folder = os.path.join(data_folder, category)
         
@@ -16,10 +24,13 @@ def main():
             mesh_path = os.path.join(category_folder, filename)
             
             if os.path.isfile(mesh_path):
-                # Load the mesh with open3d
+                '''
+                Commented stuff below is cleaner than the actual code below. 
+                Couldn't find a 'faces' attribute or anything.
+                Code below works fine, but not cool or optimal. I was lazy.
+                '''
                 # mesh = o3d.io.read_triangle_mesh(mesh_path)
                 # mesh.compute_vertex_normals()
-
                 # num_vertices = len(np.asarray(mesh.vertices))
 
                 vertices = []
@@ -41,10 +52,19 @@ def main():
                         face = [int(v.split('/')[0]) - 1 for v in parts[1:]]  
                         faces.append(face)
 
-                print(len(vertices))
-                print(len(faces))
-                
+                # Now we only store class label, num vertices and num faces. We should be more complete ofcourse
+                mesh_info.append([category, len(vertices), len(faces)])
+
+    return np.array(mesh_info)
 
 
 if __name__ == "__main__":
-    main()
+    mesh_info = read_meshes()
+
+    # Save mesh info to a CSV file
+    csv_file_path = "mesh_info.csv"
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Class', 'Vertices', 'Faces'])
+        for row in mesh_info:
+             csv_writer.writerow(row)
