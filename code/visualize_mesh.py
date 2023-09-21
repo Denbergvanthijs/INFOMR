e0256bebc1f3a94c0cce4581fe0b2650137ca980
RiemerDijkstra <50399640+RiemerDijkstra@users.noreply.github.com>
2023-09-21 12:28:31 +0200

    Vertices and faces are printed

--- code/visualize_mesh.py
+++ code/visualize_mesh.py
@@ -1,15 +1,27 @@
 import open3d as o3d
 import sys
+import pymeshlab
 
 
-def main():
-    # Obtain file name from command line input
-    mesh_path = sys.argv[1]
+# Function for retrieving number of vertices and faces of a given mesh
+def show_info(mesh_path):
+    # Obtain mesh information
+    ms = pymeshlab.MeshSet()
+    ms.load_new_mesh(mesh_path)
+    m = ms.current_mesh()
 
-    # Load the mesh with open3d
+    vertices = m.vertex_number()
+    faces = m.face_number()
+    print(vertices)
+    print(faces)
+
+
+# Function for visualizing a given mesh
+def visualize(mesh_path):
+    # Load the mesh with open3d 
     mesh = o3d.io.read_triangle_mesh(mesh_path)
     mesh.compute_vertex_normals()
-
+    
     # Visualize the mesh
     try:
         if sys.argv[2] == "shade":
@@ -20,5 +32,8 @@ def main():
         print("No acceptable visualization method was given. Please enter either 'shade' or 'wired' as an argument.")
     
    
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    # Obtain filename from command line input
+    mesh_path = sys.argv[1]
+    show_info(mesh_path)
+    visualize(mesh_path)
