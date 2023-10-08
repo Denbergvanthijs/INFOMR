1fb3bec478fb5b8244a9f792c30c5c477d99bea7
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-10-08 12:41:28 +0200

    Added error handling

--- code/patch_meshes.py
+++ code/patch_meshes.py
@@ -5,11 +5,13 @@ from pymeshlab import Mesh, MeshSet
 from tqdm import tqdm
 
 
-def refine_meshes(fp_data: str, fp_data_out: str = "data_cleaned", n_categories: int = 0) -> None:
+def refine_meshes(fp_data: str, fp_data_out: str = "data_cleaned", n_categories: int = 0) -> list:
     meshset = MeshSet()
 
     categories = next(os.walk(fp_data))[1]
     print(f"Reading {n_categories} categories from {fp_data}...")
+
+    errors = []
 
     # Iterate over all classes in the dataset (desklamp, bottle etc.)
     for category in tqdm(categories[:n_categories]):
@@ -37,16 +39,26 @@ def refine_meshes(fp_data: str, fp_data_out: str = "data_cleaned", n_categories:
             vertices = mesh.vertex_matrix()
             faces = mesh.face_matrix()
             vclean, fclean = pymeshfix.clean_from_arrays(vertices, faces)
-            mesh = Mesh(vclean, fclean)
+
+            # Check for empty mesh
+            if vclean.size == 0 or fclean.size == 0:
+                # Keep original mesh in dataset and do not overwrite the mesh variable
+                errors.append(f"{filename} has {vclean.size} vertices and {fclean.size} faces.")
+            else:
+                # Create new mesh from cleaned vertices and faces
+                mesh = Mesh(vclean, fclean)
 
             # Save cleaned mesh
             meshset.add_mesh(mesh)
             meshset.save_current_mesh(fp_mesh_out)
 
+    return errors
+
 
 if __name__ == "__main__":
     fp_data = "data"
     fp_data_out = "data_cleaned"
-    n_categories = 1  # len(categories)
+    n_categories = 10  # len(categories)
 
-    refine_meshes(fp_data=fp_data, fp_data_out=fp_data_out, n_categories=n_categories)
+    errors = refine_meshes(fp_data=fp_data, fp_data_out=fp_data_out, n_categories=n_categories)
+    print(errors)
