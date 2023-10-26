c34898650b465d787238113a09aae348ccc0d4eb
Riemer Dijkstra <riemer.dijkstra@hotmail.com>
2023-10-26 14:02:56 +0200

    updated t-sne method

--- code/dim_reduction.py
+++ code/dim_reduction.py
@@ -33,14 +33,20 @@ def main():
     # Parameters
     features_path = "csvs/feature_extraction.csv"
     tsne_no_components = 2
-    tsne_perplexity = 3
+    # Perplexity value / Sigma value (should be between 30-50 according to Alex)
+    # It accounts for the number of nearest neighbours that needs to be preserved after dim. reduction
+    tsne_perplexity = 40
 
     # Load feature vectors for db shapes
     mesh_paths, categories, features = get_features(features_path)
-    print(features[0])
 
     # Embed feature vectors in lower-dimensional space using T-distributed Stochastic Neighbor Embedding (t-SNE)
-    features_embedded = TSNE(n_components=tsne_no_components, perplexity=tsne_perplexity).fit_transform(features)
+    features_embedded = TSNE(n_components=tsne_no_components, perplexity=tsne_perplexity, random_state=42).fit_transform(features)
+
+    # Select only a couple of categories for visualization purposes
+    i = 7
+    categories = categories[:i] + categories[500:500+i] + categories[1000:1000+i] + categories[1500:1500+i]
+    features_embedded = np.vstack((features_embedded[:i], features_embedded[500:500+i], features_embedded[1000:1000+i], features_embedded[1500:1500+i]))
 
     # Create colors map
     colors_map = {}
@@ -56,11 +62,11 @@ def main():
         colors.append(colors_map[label])
     colors = np.array(colors)
 
-    # Create plot
+    # Plot category colors and labels
     fig, ax = plt.subplots()
     ax.scatter(features_embedded[:,0], features_embedded[:,1], c=colors)
-    # for x, y in zip(features_embedded[:,0], features_embedded[:,1]):
-    #     ax.annotate(path.replace("/m", "-").replace(".obj", ""), (x, y))
+    for category, x, y in zip(categories, features_embedded[:,0], features_embedded[:,1]):
+        ax.annotate(category, (x, y))
 
     plt.savefig('2D_meshes.png')
 
