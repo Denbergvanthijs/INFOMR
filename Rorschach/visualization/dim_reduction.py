fa78a0e5ba202c86c0d0e3c2596373a9438bd1ff
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-11-06 18:32:17 +0100

    Updated plots with newest data

--- Rorschach/visualization/dim_reduction.py
+++ Rorschach/visualization/dim_reduction.py
@@ -1,15 +1,19 @@
-from sklearn.manifold import TSNE
-import numpy as np
 import csv
-import open3d as o3d
-import pandas as pd
+import os
+
+import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.manifold import TSNE
 
 
-def get_features(features_path):
+def get_features(features_path: str) -> tuple:
     mesh_paths = []
     categories = []
     features = []
+
+    # Check if the file exists
+    if not os.path.isfile(features_path):
+        raise FileNotFoundError(f"Features file not found at {features_path}")
 
     with open(features_path, newline='') as file:
         csv_reader = csv.reader(file)
@@ -26,27 +30,22 @@ def get_features(features_path):
                 # The remainder of the row are the features (excluding 'volume' and 'compactness' for now)
                 features.append(row[2:3] + row[5:])
 
-    return mesh_paths, categories, np.array(features)
+    return mesh_paths, categories, np.array(features).astype(float)
 
 
-def main():
-    # Parameters
-    features_path = "./Rorschach/feature_extraction/features.csv"
-    tsne_no_components = 2
-    # Perplexity value / Sigma value (should be between 30-50 according to Alex)
-    # It accounts for the number of nearest neighbours that needs to be preserved after dim. reduction
-    tsne_perplexity = 10
-
+def main(fp_features: str, fp_save: str, tsn_no_components: int = 2, tsne_perplexity: int = 10, i: int = 7) -> None:
     # Load feature vectors for db shapes
-    mesh_paths, categories, features = get_features(features_path)
+    mesh_paths, categories, features = get_features(fp_features)
+
+    # Set NaN values to 0
+    features = np.nan_to_num(features, nan=0, posinf=0, neginf=0)
 
     # Embed feature vectors in lower-dimensional space using T-distributed Stochastic Neighbor Embedding (t-SNE)
     features_embedded = TSNE(n_components=tsne_no_components, perplexity=tsne_perplexity, random_state=42).fit_transform(features)
 
-    # Select only a couple of categories for visualization purposes
-    i = 7
     categories = categories[:i] + categories[500:500+i] + categories[1000:1000+i] + categories[1500:1500+i]
-    features_embedded = np.vstack((features_embedded[:i], features_embedded[500:500+i], features_embedded[1000:1000+i], features_embedded[1500:1500+i]))
+    to_stack = (features_embedded[:i], features_embedded[500:500+i], features_embedded[1000:1000+i], features_embedded[1500:1500+i])
+    features_embedded = np.vstack(to_stack)
 
     # Create colors map
     colors_map = {}
@@ -64,12 +63,32 @@ def main():
 
     # Plot category colors and labels
     fig, ax = plt.subplots()
-    ax.scatter(features_embedded[:,0], features_embedded[:,1], c=colors)
-    for category, x, y in zip(categories, features_embedded[:,0], features_embedded[:,1]):
+    ax.scatter(features_embedded[:, 0], features_embedded[:, 1], c=colors)
+    for category, x, y in zip(categories, features_embedded[:, 0], features_embedded[:, 1]):
         ax.annotate(category, (x, y))
 
-    plt.savefig('2D_meshes.png')
+    plt.xlabel("t-SNE component 1")
+    plt.ylabel("t-SNE component 2")
+
+    # Dont show upper and right spines
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    plt.tight_layout()
+
+    plt.savefig(fp_save, dpi=300)
 
 
 if __name__ == "__main__":
-    main()
+    fp_features = "./Rorschach/feature_extraction/features.csv"
+    fp_out = "./figures/step5/2D_meshes.png"
+
+    # Parameters
+    tsne_no_components = 2
+    # Perplexity value / Sigma value (should be between 30-50 according to Alex)
+    # It accounts for the number of nearest neighbours that needs to be preserved after dim. reduction
+    tsne_perplexity = 10
+
+    # Select only a couple of categories for visualization purposes
+    n_categories = 7
+
+    main(fp_features, fp_out, tsne_no_components, tsne_perplexity, n_categories)
