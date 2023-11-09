24ef344518292eea19d1b8e428330b24896dda41
Riemer Dijkstra <riemer.dijkstra@hotmail.com>
2023-11-09 11:07:42 +0100

    new distance function and querying

--- Rorschach/querying/query.py
+++ Rorschach/querying/query.py
@@ -29,7 +29,8 @@ def get_features(df_features, fp_mesh) -> list:
     df_temp = df_temp[df_temp["category"] == category]
 
     # Drop filename, category, volume, compactness, convexity, and rectangularity columns
-    df_temp = df_temp.drop(["filename", "category", "volume", "compactness", "convexity", "rectangularity"], axis=1)
+    # df_temp = df_temp.drop(["filename", "category", "volume", "compactness", "convexity", "rectangularity"], axis=1)
+    df_temp = df_temp.drop(["filename", "category"], axis=1)
     df_temp = df_temp.astype(float)
 
     if df_temp.empty:
@@ -58,19 +59,19 @@ def get_all_features(features_path):
                 # Second element is the category label (Humanoid, Vase, etc.)
                 categories.append(row[1])
                 # The remainder of the row are the features (excluding 'volume', 'compactness', 'convexity', and 'rectangularity' for now)
-                features.append(row[2:3] + row[5:6] + row[7:8] + row[9:])
-                # features.append(row[2:])
+                # features.append(row[2:3] + row[5:6] + row[7:8] + row[9:])
+                features.append(row[2:])
 
     return mesh_paths, categories, np.array(features).astype(float)
 
 
 # Custom distance function 
-def compute_distance(query_features, current_features, distance_function, weights=[]):
+def compute_distance(query_features, current_features, distance_function):
     # Compute distance over all elementary features
-    elementary_distance = distance_function(query_features[:6], current_features[:6])
+    elementary_distance = distance_function(query_features[:7], current_features[:7])
     # Compute EMD over all shape property features (histograms)
-    shape_distance = get_emd(query_features[6:], current_features[6:])
-    total_distance = elementary_distance + shape_distance
+    shape_distance = get_emd(query_features[7:], current_features[7:])
+    total_distance = elementary_distance/10 + shape_distance*10
     return total_distance
 
 
@@ -89,7 +90,6 @@ def visualize(fp_meshes: str, width: int = 1280, height: int = 720,
         meshes.append(mesh)
 
     o3d.visualization.draw_geometries(meshes, width=width, height=height,
-                                      mesh_show_wireframe=mesh_show_wireframe,
                                       window_name=window_name, mesh_show_back_face=mesh_show_back_face)
 
 
@@ -109,7 +109,7 @@ def get_k_closest(query_features: np.ndarray, features: np.ndarray, k: int, dist
     :rtype: tuple
     """
     # List of distances between query mesh and all other meshes in the dataset
-    scores = [distance_function(query_features, feature) for feature in features]
+    scores = [compute_distance(query_features, feature, distance_function) for feature in features]
     scores = np.array(scores, dtype=float)
     indices = np.arange(len(scores))
 
@@ -140,14 +140,14 @@ def return_dist_func(selector: str):
 
 if __name__ == "__main__":
     # Query shape/mesh
-    fp_query = "./data_normalized/Bird/D00089.obj"
-    fp_features = "./Rorschach/feature_extraction/features.csv"
+    fp_query = "./data_normalized/Knife/D01077.obj"
+    fp_features = "./Rorschach/feature_extraction/features_new.csv"
     fp_data = "./data_normalized/"
-    k = 3  # Number of nearest neighbours to retrieve
+    k = 5  # Number of nearest neighbours to retrieve
 
-    distance_function = get_emd
+    distance_function = get_euclidean_distance
     # Flag for using KNN instead of custom distance functions
-    knn = True
+    knn = False
 
     # Retrieve features from the returned meshes
     df_features = pd.read_csv(fp_features)
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     df_features["filename"] = df_features["filename"].apply(lambda x: x.split("/")[-1])
 
     features_query = get_features(df_features, fp_query)  # First get features, before dropping columns
-    df_features = df_features.drop(["volume", "compactness", "convexity", "rectangularity"], axis=1)
+    # df_features = df_features.drop(["volume", "compactness", "convexity", "rectangularity"], axis=1)
 
     # Get all features from the dataset
     filepaths, categories, features = get_all_features(fp_features)
@@ -181,11 +181,12 @@ if __name__ == "__main__":
 
     else:  # Use custom distance functions for querying
         # Create an ordered list of meshes retrieved from the dataset based on EMD (with respect to the query mesh)
+        print(len(features_query))
         sorted_scores, sorted_indices = get_k_closest(features_query, features, k=k, distance_function=distance_function)
         returned_meshes = ["./data_normalized/" + filepaths[i] for i in sorted_indices]
 
         print(f"Number of returned meshes: {len(returned_meshes)}")
-        print(f"Best match: {returned_meshes[0]} with EMD: {sorted_scores[0]:3f}")
+        print(f"Best match: {returned_meshes[0]} with distance: {sorted_scores[0]:3f}")
 
         # Visualize query mesh and desired mesh from returned mesh list (index 0: best match, index -1: worst match)
         meshes_to_visualize = [fp_query] + returned_meshes
