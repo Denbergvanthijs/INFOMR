09ce89ddd0e8f082a6d4463082aa159452e8ceaa
Riemer Dijkstra <riemer.dijkstra@hotmail.com>
2023-11-17 16:03:00 +0100

    added convex hull to MeshViewer.py

--- Rorschach/visualization/mesh_viewer.py
+++ Rorschach/visualization/mesh_viewer.py
@@ -1,17 +1,18 @@
 import argparse
-
 import open3d as o3d
-import pymeshlab
+from pymeshlab import MeshSet
+
 
 parser = argparse.ArgumentParser(description="Visualize a given mesh.")
 parser.add_argument("--mesh_path", type=str, dest="mesh_path",
-                    default="./data/Spoon/D00014.obj", help="Path to the mesh file.")
-parser.add_argument("--visualization_method", type=str, dest="visualization_method",
-                    default="shade", help="Visualization method. Either 'shade' or 'wired'.", choices=["shade", "wired"])
+                    default="./data/Quadruped/D00409.obj", help="Path to the mesh file.")
+parser.add_argument("--method", type=str, dest="method",
+                    default="standard", help="Visualization method ('standard', 'convex_hull', 'axes')")
 
 
 def show_info(mesh_path: str) -> (int, int):
-    """Function for retrieving number of vertices and faces of a given mesh.
+    """
+    Function for retrieving number of vertices and faces of a given mesh.
 
     :param mesh_path: Path to the mesh file
     :type mesh_path: str
@@ -20,52 +21,72 @@ def show_info(mesh_path: str) -> (int, int):
     :rtype: (int, int)
     """
     # Obtain mesh information
-    meshset = pymeshlab.MeshSet()
+    meshset = MeshSet()
     meshset.load_new_mesh(mesh_path)
     mesh_current = meshset.current_mesh()
 
     return mesh_current.vertex_number(), mesh_current.face_number()
 
 
+# Function for constructing the convex hull of a mesh
+def compute_convex_hull(mesh_path):
+    meshset = MeshSet()
+    meshset.load_new_mesh(mesh_path)
+    meshset.generate_convex_hull()
+    hull_path = 'convex_hull.obj'
+    meshset.save_current_mesh(hull_path)
+    return hull_path
+
+
 # Function for visualizing a given mesh
-def visualize(mesh_path: str, visualization_method: str, width: int = 1280, height: int = 720) -> None:
-    """Function for visualizing a given mesh.
+def visualize(mesh_path: str, width: int = 1280, height: int = 720, method: str = 'standard') -> None:
+    """
+    Function for visualizing a given mesh.
 
     :param mesh_path: Path to the mesh file
     :type mesh_path: str
-    :param visualization_method: Visualization method. Either 'shade' or 'wired'.
-    :type visualization_method: str
-
-    :raises IndexError: If no acceptable visualization method was given
 
     :return: None
     :rtype: None
     """
-    # Load the mesh with open3d
-    mesh = o3d.io.read_triangle_mesh(mesh_path)
+    # Load mesh with open3d
+    mesh = o3d.io.read_triangle_mesh(args.mesh_path)
     mesh.compute_vertex_normals()
 
     window_name = f"Rorschach - Visualized mesh: {mesh_path}"
 
     # Draw cartesian frame of reference
     mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1)
-    mesh_frame.translate([0, 0, 0])
+    mesh_frame.translate([0,0,0])
+    
+    # Visualize mesh and its convex hull
+    if method == 'convex_hull':
+        hull_path = compute_convex_hull(mesh_path)
+        convex_hull = o3d.io.read_triangle_mesh(hull_path)
+        convex_hull.compute_vertex_normals()
+        # convex_hull.paint_uniform_color(1, 0, 0, 0.3)
+        o3d.visualization.draw_geometries([mesh, convex_hull], width=width, height=height, window_name=window_name)
 
-    # Visualize the mesh
-    if visualization_method == "shade":
-        o3d.visualization.draw_geometries([mesh], width=width, height=height, window_name=window_name)
-    elif visualization_method == "wired":
-        o3d.visualization.draw_geometries([mesh], width=width, height=height, window_name=window_name, mesh_show_wireframe=True)
+    # Visualize mesh and 3D axes (cartesian frame)
+    elif method == 'axes':
+        o3d.visualization.draw_geometries([mesh, mesh_frame], width=width, height=height, window_name=window_name)
+
+    # Only visualize the original mesh
     else:
-        raise IndexError("No acceptable visualization method was given. Please enter either 'shade' or 'wired' as an argument.")
+        o3d.visualization.draw_geometries([mesh], width=width, height=height, window_name=window_name)
 
 
 if __name__ == "__main__":
-    # Example command: python ./Rorschach/visualization/mesh_viewer.py --mesh_path ./data/AircraftBuoyant/m1337.obj --visualization_method shade
+    # Obtain filename from command line input
+    # Example command: python Rorschach/visualization/MeshViewer.py --mesh_path ./data/Spoon/D00014.obj 
     args = parser.parse_args()
+    mesh_path = args.mesh_path
+    method = args.method
 
-    vertices, faces = show_info(args.mesh_path)
+    # Print mesh info
+    vertices, faces = show_info(mesh_path)
     print(f"Number of vertices: {vertices}")
     print(f"Number of faces: {faces}")
 
-    visualize(args.mesh_path, args.visualization_method)
+    # Visualize the mesh either with or without its convex hull, and with or without 3D axes
+    visualize(mesh_path, method=method)
